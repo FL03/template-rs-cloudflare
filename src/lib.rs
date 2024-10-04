7473ff9732bcf69a45b1a5ea0ff209d0ed965f92
FL03 <jo3mccain@icloud.com>
2024-10-04 14:06:51 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,51 @@
     Appellation: template-rs-cloudflare <library>
     Contrib: FL03 <jo3mccain@icloud.com>
 */
+use axum::{routing::get, Router};
+use tower_service::Service;
 use worker::*;
 
+fn router() -> Router {
+    Router::new().route("/", get(root))
+}
+
 #[event(fetch)]
-pub async fn main(_req: Request, _env: Env, _ctx: worker::Context) -> Result<Response> {
-    
-    let data = Message {
-        hello: String::from("world"),
-    
-    };
-    Response::from_json(&data)
+async fn fetch(
+    req: HttpRequest,
+    _env: Env,
+    _ctx: Context,
+) -> Result<axum::http::Response<axum::body::Body>> {
+    #[cfg(target_family = "wasm")]
+    console_error_panic_hook::set_once();
+    Ok(router().call(req).await?)
+}
+
+pub async fn root() -> &'static str {
+    let message = Message::new("Hello, World!");
+    console_log!("{}", message);
+    "Hello, World!"
 }
 
 
-#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, serde::Deserialize, serde::Serialize)]
+#[derive(Clone, Default, Eq, Hash, Ord, PartialEq, PartialOrd, serde::Deserialize, serde::Serialize)]
 pub struct Message {
-    hello: String,
+    message: String,
 }
 
 impl Message {
-    pub fn new(hello: impl ToString) -> Self {
-        Self { hello: hello.to_string() }
+    pub fn new(message: impl ToString) -> Self {
+        Self { message: message.to_string() }
+    }
+}
+
+impl core::fmt::Debug for Message {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.write_str(serde_json::to_string_pretty(self).unwrap().as_str())
+    }
+}
+
+impl core::fmt::Display for Message {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        f.write_str(serde_json::to_string(self).unwrap().as_str())
     }
 }
