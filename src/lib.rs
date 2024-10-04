498099a1aee41ac1d2813f3df19b70db4dff1898
FL03 <jo3mccain@icloud.com>
2024-10-04 13:40:03 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,45 +1,27 @@
-
-use serde::{Deserialize, Serialize};
+/*
+    Appellation: template-rs-cloudflare <library>
+    Contrib: FL03 <jo3mccain@icloud.com>
+*/
 use worker::*;
 
-#[derive(Deserialize, Serialize, Debug)]
-struct Json {
+#[event(fetch)]
+pub async fn main(_req: Request, _env: Env, _ctx: worker::Context) -> Result<Response> {
+    
+    let data = Message {
+        hello: String::from("world"),
+    
+    };
+    Response::from_json(&data)
+}
+
+
+#[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd, serde::Deserialize, serde::Serialize)]
+pub struct Message {
     hello: String,
 }
 
-
-
-
-#[event(fetch)]
-pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Response> {
-    console_log!(
-        "{} {}, located at: {:?}, within: {}",
-        req.method().to_string(),
-        req.path(),
-        req.cf().unwrap().coordinates().unwrap_or_default(),
-        req.cf().unwrap().region().unwrap_or("unknown region".into())
-    );
-
-    if !matches!(req.method(), Method::Post) {
-        return Response::error("Method Not Allowed", 405);
+impl Message {
+    pub fn new(hello: impl ToString) -> Self {
+        Self { hello: hello.to_string() }
     }
-
-    if let Some(file) = req.form_data().await?.get("file") {
-        return match file {
-            FormEntry::File(buf) => {
-                Response::ok(&format!("size = {}", buf.bytes().await?.len()))
-            }
-            _ => Response::error("`file` part of POST form must be a file", 400),
-        };
-    }
-
-    Response::error("Bad Request", 400)
-}
-
-#[event(fetch)]
-async fn fetch(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
-    let data = Json {
-        hello: String::from("world"),
-    };
-    Response::from_json(&data)
 }
