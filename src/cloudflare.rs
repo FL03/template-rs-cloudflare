b78fb558ca668f4ce963caa69ced2486b2e895e4
FL03 <jo3mccain@icloud.com>
2025-05-18 12:40:49 -0500

    update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- src/cloudflare.rs
+++ src/cloudflare.rs
@@ -9,6 +9,7 @@ use worker::{Context, Env, HttpRequest};
 /// the primary entry point for the worker
 #[worker::event(fetch)]
 pub async fn fetch(req: HttpRequest, _env: Env, _ctx: Context) -> worker::Result<BodyResponse> {
+    #[cfg(feature = "console_error_panic_hook")]
     console_error_panic_hook::set_once();
     Ok(crate::api().call(req).await?)
 }
